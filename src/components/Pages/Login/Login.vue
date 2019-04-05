6079e70c5171b69cd42b272d53d01282aea4559a
AmosHuKe <amoshuke@qq.com>
2019-04-05 22:40:17 +0800

    Login

--- src/components/Pages/Login/Login.vue
+++ src/components/Pages/Login/Login.vue
@@ -1,6 +1,99 @@
 <template>
     <div id="Login">
-        <h2 @click="loginDemo()">{{ titleT }}</h2>
+        <div class="container-scroller" id="aplogn">
+            <div class="container-fluid page-body-wrapper full-page-wrapper">
+                <div class="content-wrapper d-flex align-items-center auth">
+                    <div class="row w-100">
+                        <div class="col-lg-4 mx-auto">
+                            <!-- Main -->
+                            <div class="auth-form-light text-left p-5">
+                                <div class="brand-logo display-4">  
+                                    {{ tTitle }}
+                                </div>
+                                <h6 class="font-weight-light"></h6>
+                                
+                                <!-- 普通登陆 -->
+                                <form class="pt-3 userloginf" v-show="loginShow">
+                                    <div class="form-group">
+                                        <input
+                                            type="text"
+                                            class="form-control form-control-lg oBorder"
+                                            id="mUserName"
+                                            v-model="mUserName"
+                                            :placeholder="userName_Pla">
+                                    </div>
+                                    <div class="form-group">
+                                        <input 
+                                            type="password"
+                                            class="form-control form-control-lg oBorder"
+                                            id="mPassWord"
+                                            v-model="mPassWord"
+                                            :placeholder="passWord_Pla">
+                                    </div>
+
+                                    <div class="mt-3">
+                                    
+                                        <button
+                                            type="button"
+                                            :class="submitL ? 'btn btn-block btn-gradient-primary btn-lg auth-form-btn buttonBorder' : 'btn btn-block btn-gradient-primary btn-lg auth-form-btn buttonBorder buttonLoading'"
+                                            href="javascript:;"
+                                            @click="submitLogin($event)">
+                                            登 录</button>
+                                    
+                                    </div>
+                                <div class="my-2 d-flex justify-content-between align-items-center">
+                                    <div class="form-check">
+                                        <a href="javascript:;" class="auth-link text-black text-primary" @click="Showlogin()"><i class="mdi mdi-email-variant "></i> 短信登陆</a>
+                                    </div>
+                                    </div>
+                                </form>
+                                <!-- End普通登陆 -->
+                                
+                                <!-- 短信登陆 -->
+                                <form class="pt-3 smsloginf" name="loginf" method="post" action="" v-show="smsShow">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <input 
+                                                type="text" 
+                                                name="tphone" 
+                                                class="form-control form-control-lg oBorder" 
+                                                id="tphone" v-model="tphone" 
+                                                @input="inputM()" 
+                                                placeholder="手机号">
+                                        </div>
+                                    
+                                    </div>
+                                    <div class="form-group" v-show="yzmShow">
+                                        <input 
+                                            type="text" 
+                                            name="tyzm" 
+                                            class="form-control form-control-lg oBorder" 
+                                            id="tyzm" 
+                                            v-model="yzm" 
+                                            placeholder="验证码">
+                                    </div>
+                                    <div class="mt-3">
+                                        <button
+                                            type="button"
+                                            :class="submitLsms ? 'btn btn-block btn-gradient-primary btn-lg auth-form-btn buttonBorder' : 'btn btn-block btn-gradient-primary btn-lg auth-form-btn buttonBorder buttonLoading'"
+                                            href="javascript:;"
+                                            @click="submitLogin($event)">登 录</button>
+                                    </div>
+                                    <div class="my-2 d-flex justify-content-between align-items-center">
+                                        <div class="form-check">
+                                            <a href="javascript:;" class="auth-link text-black userlogin text-primary" @click="Showlogin()"><i class="mdi mdi-account-circle"></i> 账号登陆</a>
+                                        </div>
+                                    </div>
+                                    
+                                </form>
+                                <!-- End短信登陆 -->
+                            </div>
+                            <!-- Main End-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,21 +106,47 @@ export default {
     name: 'Login',
     data () {
         return {
-            titleT: '登陆',
-            username: 'admin',
-            password:  'admin',
+            tTitle: 'Vue-Test' //标题
+
+            //普通账号登录
+            ,mUserName : '' //账号
+            ,userName_Pla : '账号' //账号提示框
+            ,mPassWord : '' //密码
+            ,passWord_Pla : '密码' //密码提示框
+            
+            ,submitL : true //登录操作锁
+            
+            ,loginShow: true //普通登陆是否隐藏
+            ,smsShow: false //短信登陆是否隐藏
+            ,yzmShow: false //短信验证码是否隐藏
+            
+            
+            //短信登陆
+            ,tphone:"" //手机号
+            ,yzm:"" //验证码
+            ,submitLsms : true //sms登录操作锁
         }
     },
     methods: {
-        //登陆测试
-        loginDemo() {
-            getToken(this.username, this.password)
+        submitLogin() {
+            //登录
+            getToken(this.mUserName, this.mPassWord)
             .then((response)=>{
                 alert(JSON.stringify(response))
             })
                 
+        },
+        Showlogin(){
+            //普通账户和短信登录切换
+            this.loginShow = !this.loginShow
+            this.smsShow = !this.smsShow
         }
     }
     
 }
 </script>
+
+<style scoped>
+    /* 登录样式 */
+    @import url("./css/Login.css"); 
+</style>
