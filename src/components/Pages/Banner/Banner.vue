786c0aba21cbd69f5a12c7a1f64b2e3434a7b575
AmosHuKe <amoshuke@qq.com>
2019-05-02 17:56:12 +0800

    [修改] Banner样式

--- src/components/Pages/Banner/Banner.vue
+++ src/components/Pages/Banner/Banner.vue
@@ -3,8 +3,22 @@
         <!-- 首页相关展示 -->
         
             <!-- 走马灯 -->
-            <a-carousel autoplay>
-                
+            <a-carousel arrows autoplay>
+                <div
+                    slot="prevArrow"
+                    class="custom-slick-arrow"
+                    style="left: 10px;zIndex: 1"
+                >
+                    <a-icon type="left-circle" />
+                </div>
+                <div
+                    slot="nextArrow" 
+                    class="custom-slick-arrow"
+                    style="right: 10px"
+                >
+                    <a-icon type="right-circle" />
+                </div>
+
                 <div v-for="item in 4" :key="item">               
                     <h3>😄 {{ msg }} {{ item }} </h3>  
                 </div>
@@ -28,14 +42,27 @@ export default {
 
 <style lang="less" scoped>
     @Lheight: 420px;
+    @iconSize: 24px;
     .ant-carousel {
         text-align: center;
         height: @Lheight;
         line-height: @Lheight;
         background: #364d79;
         overflow: hidden;
-        .slick-slide h3 {
-            color: #fff;
+        .slick-slide{
+            h3 {color: #fff;}      
         }
+        .custom-slick-arrow {
+            width: @iconSize;
+            height: @iconSize;
+            font-size: @iconSize;
+            color: #fff;
+            background-color: rgba(31,45,61,.11);
+            opacity: 0.3;
+
+            &:hover {opacity: 0.5;}
+            &:before {display: none;}
+        }
+
     }
 </style>
