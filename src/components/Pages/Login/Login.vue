ad6c66fd290cc993a64a2e67e8a2771447d93e73
AmosHuKe <amoshuke@qq.com>
2019-05-14 09:22:01 +0800

    Update Login.vue

--- src/components/Pages/Login/Login.vue
+++ src/components/Pages/Login/Login.vue
@@ -4,7 +4,7 @@
             <div class="container-fluid page-body-wrapper full-page-wrapper">
                 <div class="content-wrapper d-flex align-items-center auth">
                     <div class="row w-100">
-                        <div class="col-lg-4 mx-auto">
+                        <div class="col-lg-4 mx-auto" >
                             <!-- Main -->
                             <div class="auth-form-light text-left p-5">
                                 <div class="brand-logo display-4">  
